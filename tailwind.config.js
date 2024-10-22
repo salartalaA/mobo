5faa74f9b14a0adfc1c7e4c4e95218b7a65a6a5a
salartalaA <salarmhmdn@gmail.com>
2024-10-22 19:57:01 +0330

    first project using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      'sm': '739px',
+
+      'md': '828px',
+
+      'lg': '1120px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
